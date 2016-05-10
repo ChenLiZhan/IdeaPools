13790c442ccf1d40f0501d4381b5b4285c8da14a
chenlizhan <chung1350@hotmail.com>
2016-05-10 17:05:28 +0800

    condition1 prototype

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -19,6 +19,26 @@ app.config(['$stateProvider', '$urlRouterProvider', '$controllerProvider', '$loc
       templateUrl: 'partials/condition1-1.html',
       controller: 'Condition1'
     })
+    .state('condition1-2', {
+      url: '/condition1/2',
+      templateUrl: 'partials/condition1-2.html',
+      controller: 'Condition1'
+    })
+    .state('condition1-3', {
+      url: '/condition1/3',
+      templateUrl: 'partials/condition1-3.html',
+      controller: 'Condition1'
+    })
+    .state('condition1-4', {
+      url: '/condition1/4',
+      templateUrl: 'partials/condition1-4.html',
+      controller: 'Condition1'
+    })
+    .state('condition1-final', {
+      url: '/condition1/final',
+      templateUrl: 'partials/condition1-final.html',
+      controller: 'Condition1'
+    })
     .state('condition2', {
       url: '/condition2',
       templateUrl: 'partials/condition2.html',
--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -16,13 +16,33 @@ function Index($rootScope, $scope) {
 
 Index.$inject = ['$rootScope', '$scope'];
 
-function Condition1($rootScope, $scope, $location) {
+function Condition1($rootScope, $scope, $location, $window) {
   $scope.start = function() {
     $location.path('/condition1/1');
-  }
+  };
+
+  $scope.goSecond = function() {
+    $location.path('/condition1/2');
+  };
+
+  $scope.goThird = function() {
+    $location.path('/condition1/3');
+  };
+
+  $scope.goBrainStorming = function() {
+    $location.path('/condition1/4');
+  };
+
+  $scope.goFinal = function() {
+    $location.path('/condition1/final');
+  };
+
+  $scope.goForm = function() {
+    $window.location.href = 'http://google.com'
+  };
 }
 
-Condition1.$inject = ['$rootScope', '$scope', '$location'];
+Condition1.$inject = ['$rootScope', '$scope', '$location', '$window'];
 
 function Condition2($rootScope, $scope) {}
 
